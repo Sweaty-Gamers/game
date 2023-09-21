fedf25ecd5a34ac57dde28a425504090c353ed35
Tyler Nijmeh <tylernij@gmail.com>
2023-09-21 14:56:07 -0700

    Add reload
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WeaponScript : MonoBehaviour
@@ -8,8 +10,11 @@ public class WeaponScript : MonoBehaviour
     public float reloadTime;
     public int magSize;
     public float bulletSpeed;
-
     public GameObject bulletPrefab;
+
+    public float reserveBullets;
+    private float bulletsLeftInMag = 0;
+    private bool isReloading = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +29,53 @@ public class WeaponScript : MonoBehaviour
     }
 
     void Shoot() {
+        if (isReloading) return;
+
+        if (bulletsLeftInMag <= 0 && reserveBullets > 0) {
+            StartCoroutine(Reload());
+            return;
+        } else if (bulletsLeftInMag <= 0) {
+            return;
+        }
+
         Vector3 screenSpaceCenter = new(0.5f, 0.5f, 1);
         Vector3 screenCenter = Camera.main.ViewportToWorldPoint(screenSpaceCenter);
         GameObject bullet = Instantiate(bulletPrefab, screenCenter, Quaternion.identity);
         bullet.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * bulletSpeed;
 
         StartCoroutine(DespawnBullet(bullet));
+
+        bulletsLeftInMag--;
+    }
+
+    IEnumerator Reload()
+    {
+        if (bulletsLeftInMag == magSize) yield break;
+
+        isReloading = true;
+        // TODO: play animation
+        yield return new WaitForSeconds(reloadTime);
+
+        /*
+            MAG     RESERVE
+            3       100
+            *reload*
+            20      87
+
+            3       10
+            *reload*
+            13      0
+        */
+
+        float bulletsToLoad = Math.Min(
+            magSize - bulletsLeftInMag,
+            reserveBullets
+        );
+
+        reserveBullets -= bulletsToLoad;
+        bulletsLeftInMag += bulletsToLoad;
+
+        isReloading = false;
     }
 
     // Update is called once per frame
