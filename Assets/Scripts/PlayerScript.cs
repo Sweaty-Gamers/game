0c4513c6afd11119465482fbaf7177e91ef372d0
Tyler Nijmeh <tylernij@gmail.com>
2023-09-21 16:44:44 -0700

    Rename playermovement
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
     public float movementSpeed;
     public float jumpForce;
