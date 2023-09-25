456e66f60c2e26b50ab56efe4bea35c31784b408
Michael Truong <truong.minh.michael@gmail.com>
2023-09-24 20:46:49 -0700

    Added enemy pathfinding

---
+++ Assets/Scripts/EnemyPathfindScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPathfindScript : MonoBehaviour
+{
+
+    public Transform player;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = player.position;
+    }
+}
