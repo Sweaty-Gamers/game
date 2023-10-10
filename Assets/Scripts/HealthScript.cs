3b7d7cc94e64c9c6a570260ec3190a9f415b6e53
Tyler Nijmeh <tylernij@gmail.com>
2023-10-10 16:14:51 -0700

    Add health script
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour {
+    /// Health points.
+    public float health;
+
+    /// Max health points;
+    public float maxHealth;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
