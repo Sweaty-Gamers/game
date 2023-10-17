3ed0d7c6920e9c321976da1ba4ed2c1177fc25f3
ItzUzi <ugaeta@cpp.edu>
2023-10-17 03:25:43 -0700

    Initialized value for start scene

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -12,7 +12,7 @@ public class TitleScreen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        startGameScene = "New Scene";
     }
 
     // Update is called once per frame
