abd1e1533d3bcda7b8b600552b2a282c4dd57b0a
ItzUzi <ugaeta@cpp.edu>
2023-11-07 14:47:48 -0800

    replaced health script with dragon script

--- Assets/Scripts/DragonScript.cs
+++ Assets/Scripts/DragonScript.cs
@@ -3,7 +3,7 @@ using System;
 public class DragonScript : Enemy
 {
     public static float newHealth = 700f;
-    public static float healthCap = 1000;
+    public static float healthCap = 1000f;
     public MeleeWeapon fire;
     public float damage = 10f;
     public float knockBack = .5f;
